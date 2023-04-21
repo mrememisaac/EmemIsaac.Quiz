5a0a3af7fe02a70debe177831ce4655504bcf0ee
Emem Isaac <me@ememisaac.com>
2023-04-21 19:35:15 +0100

    Adds teh get subjects query feature

---
+++ EmemIsaac.Quiz.Application/Features/Subjects/GetSubjectsQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace EmemIsaac.Quiz.Application.Features.Subjects;
+
+public class GetSubjectsQuery : IRequest<List<SubjectVM>>
+{
+    public string? CreatorUserId { get; set; }
+}
---
+++ EmemIsaac.Quiz.Application/Features/Subjects/SubjectVM.cs
@@ -0,0 +1,13 @@
+﻿namespace EmemIsaac.Quiz.Application.Features.Subjects;
+
+public class SubjectVM
+{
+    public Guid SubjectId { get; set; }
+
+    public string SubjectName { get; set; } = string.Empty;
+
+    public string SubjectDescription { get; set; } = string.Empty;
+
+    public string SubjectImageUri { get; set; } = string.Empty;
+
+}
