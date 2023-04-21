e51a45845e4a88aa4f581090848dae93c1e8fa9a
Emem Isaac <me@ememisaac.com>
2023-04-21 19:37:29 +0100

    Adds a base class for audit worthy entities

---
+++ EmemIsaac.Quiz.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmemIsaac.Quiz.Domain.Common
+{
+    public class AuditableEntity
+    {
+        public string? CreatedBy { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public string? LastModifiedBy { get; set; }
+
+        public DateTime? DateLastModified { get; set; }
+
+    }
+}
