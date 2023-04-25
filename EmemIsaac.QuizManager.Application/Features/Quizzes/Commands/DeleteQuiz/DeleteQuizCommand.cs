5875295f5424c7fa25dcf765c576ca8f676ef8f7
Emem Isaac <me@ememisaac.com>
2023-04-25 18:18:27 +0100

    Implements the delete quiz feature

---
+++ EmemIsaac.QuizManager.Application/Features/Quizzes/Commands/DeleteQuiz/DeleteQuizCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace EmemIsaac.QuizManager.Application.Features.Quizzes.Commands.DeleteQuiz
+{
+    public class DeleteQuizCommand : IRequest<DeleteQuizCommandResponse>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ EmemIsaac.QuizManager.Application/Features/Quizzes/Commands/DeleteQuiz/DeleteQuizCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using EmemIsaac.QuizManager.Application.Contracts.Persistence;
+using MediatR;
+
+namespace EmemIsaac.QuizManager.Application.Features.Quizzes.Commands.DeleteQuiz
+{
+    public class DeleteQuizCommandHandler : IRequestHandler<DeleteQuizCommand, DeleteQuizCommandResponse>
+    {
+        private readonly IQuizzesRepository _quizzesRepository;
+        
+        private readonly IMapper _mapper;
+
+        public DeleteQuizCommandHandler(IQuizzesRepository quizzesRepository, IMapper mapper)
+        {
+            _quizzesRepository = quizzesRepository;
+            _mapper = mapper;
+        }
+        
+        public async Task<DeleteQuizCommandResponse> Handle(DeleteQuizCommand request, CancellationToken cancellationToken)
+        {
+            var quiz = await _quizzesRepository.GetByIdAsync(request.Id);
+            await _quizzesRepository.DeleteAsync(quiz);
+            return new DeleteQuizCommandResponse();
+        }
+    }
+}
