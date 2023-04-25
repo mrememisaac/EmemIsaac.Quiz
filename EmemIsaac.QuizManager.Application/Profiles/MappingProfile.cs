e7a1ae20b5ed73acad8582ca8272ead09f7a179c
Emem Isaac <me@ememisaac.com>
2023-04-25 16:11:37 +0100

    Adds basic mapping profiles

---
+++ EmemIsaac.QuizManager.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using EmemIsaac.QuizManager.Application.Features.Options.Queries.GetQuestionOptions;
+using EmemIsaac.QuizManager.Application.Features.Questions.Queries.GetQuizQuestions;
+using EmemIsaac.QuizManager.Application.Features.Quizzes.Commands.CreateQuiz;
+using EmemIsaac.QuizManager.Application.Features.Quizzes.Commands.UpdateQuiz;
+using EmemIsaac.QuizManager.Application.Features.Quizzes.Queries.GetUsersQuizzes;
+using EmemIsaac.QuizManager.Domain.Entities;
+
+namespace EmemIsaac.QuizManager.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Option, OptionVM>().ReverseMap();
+            CreateMap<Question, QuestionVM>().ReverseMap();
+            CreateMap<Quiz, QuizVM>().ReverseMap();
+            CreateMap<CreateQuizCommand, Quiz>();
+            CreateMap<UpdateQuizCommand, Quiz>();
+        }
+    }
+}
