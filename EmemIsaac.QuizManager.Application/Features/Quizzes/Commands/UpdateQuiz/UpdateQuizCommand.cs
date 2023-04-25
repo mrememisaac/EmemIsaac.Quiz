82fcf7ae1786fe012b6b3cc4bad51754762711a4
Emem Isaac <me@ememisaac.com>
2023-04-25 16:09:46 +0100

    implements the update quiz feature

---
+++ EmemIsaac.QuizManager.Application/Features/Quizzes/Commands/UpdateQuiz/UpdateQuizCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace EmemIsaac.QuizManager.Application.Features.Quizzes.Commands.UpdateQuiz
+{
+    public class UpdateQuizCommand : IRequest
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string ImageUri { get; set; } = string.Empty;
+    }
+}
---
+++ EmemIsaac.QuizManager.Application/Features/Quizzes/Commands/UpdateQuiz/UpdateQuizCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using EmemIsaac.QuizManager.Application.Contracts.Persistence;
+using MediatR;
+
+namespace EmemIsaac.QuizManager.Application.Features.Quizzes.Commands.UpdateQuiz
+{
+    public class UpdateQuizCommandHandler : IRequestHandler<UpdateQuizCommand>
+    {
+        private readonly IQuizzesRepository _quizzesRepository;
+        private readonly IMapper _mapper;
+
+        public UpdateQuizCommandHandler(IQuizzesRepository quizzesRepository, IMapper mapper)
+        {
+            _quizzesRepository= quizzesRepository;
+            _mapper= mapper;
+        }
+
+        public async Task Handle(UpdateQuizCommand request, CancellationToken cancellationToken)
+        {
+            var quiz = await _quizzesRepository.GetByIdAsync(request.Id);
+            _mapper.Map(request, quiz, typeof(UpdateQuizCommand), typeof(UpdateQuizCommand));
+            await _quizzesRepository.UpdateAsync(quiz);
+            return Unit.Value;
+        }
+    }
+}
