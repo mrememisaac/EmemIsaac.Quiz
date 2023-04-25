4aecc241617dd788e835357b20dab5d849b5028d
Emem Isaac <me@ememisaac.com>
2023-04-25 18:19:26 +0100

    Adds the base response type

---
+++ EmemIsaac.QuizManager.Application/Responses/BaseResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmemIsaac.QuizManager.Application.Responses
+{
+    public class BaseResponse
+    {
+        public bool Success { get; }
+
+        public string Message { get; } = string.Empty;
+
+        public IReadOnlyList<string>? ValidationErrors { get; }
+
+        public BaseResponse(bool success = true, string message = "") { 
+            Success = success; 
+            Message = message;
+        }
+
+        public BaseResponse(IList<string> errors)
+        {
+            Success = false;
+            ValidationErrors = new List<string>(errors);
+        }
+    }
+}
