f0ab7941a43b73ee53d59f263cac05cb59e47137
Emem Isaac <me@ememisaac.com>
2023-04-25 18:18:07 +0100

    Updates the create quiz feature implementation

---
+++ EmemIsaac.QuizManager.Application/Features/Quizzes/Commands/CreateQuiz/CreateQuizCommandResponse.cs
@@ -0,0 +1,17 @@
+﻿using EmemIsaac.QuizManager.Application.Responses;
+
+namespace EmemIsaac.QuizManager.Application.Features.Quizzes.Commands.CreateQuiz
+{
+    public class CreateQuizCommandResponse : BaseResponse
+    {
+        public CreateQuizDto? Quiz { get;  }
+
+        public CreateQuizCommandResponse(bool success = true, string message = "", CreateQuizDto? quiz = null) : base(success, message) 
+        {
+            Quiz = quiz;
+        }
+
+        public CreateQuizCommandResponse(IList<string> errors) : base(errors) { }
+
+    }
+}
