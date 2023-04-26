b4f916cd82ed83ec32c1aa2c7985beb96f2c1fde
Emem Isaac <me@ememisaac.com>
2023-04-26 17:23:13 +0100

    Configures services and pipelines

---
+++ EmemIsaac.QuizManager.Api/Program.cs
@@ -0,0 +1,9 @@
+using EmemIsaac.QuizManager.Api;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder
+    .ConfigureServices()
+    .ConfigurePipeline();
+
+app.Run();
---
+++ EmemIsaac.QuizManager.Api/StartupExtensions.cs
@@ -0,0 +1,35 @@
+﻿using EmemIsaac.QuizManager.Application;
+using EmemIsaac.QuizManager.Persistence;
+
+namespace EmemIsaac.QuizManager.Api
+{
+    public static class StartupExtensions
+    {
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddApplicationServices();
+            builder.Services.AddPersistenceServices(builder.Configuration);
+
+            builder.Services.AddControllers();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("Open", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            });
+
+            return builder.Build();
+        }
+
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors("Open");
+
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
