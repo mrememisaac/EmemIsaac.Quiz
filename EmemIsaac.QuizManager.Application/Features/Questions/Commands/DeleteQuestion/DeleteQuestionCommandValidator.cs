8f5e948e2eeecb372de6df6c53ba302737d1e6c9
Emem Isaac <me@ememisaac.com>
2023-09-27 20:40:27 +0100

    Adds the delete question command validator

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Commands/DeleteQuestion/DeleteQuestionCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace EmemIsaac.QuizManager.Application.Features.Questions.Commands.DeleteQuestion
+{
+    public class DeleteQuestionCommandValidator : AbstractValidator<DeleteQuestionCommand>
+    {
+        public DeleteQuestionCommandValidator()
+        {
+            RuleFor(p => p.QuestionId)
+               .NotEmpty().WithMessage(Common.ValidationMessages.RequiredMessage);
+        }
+    }
+}
