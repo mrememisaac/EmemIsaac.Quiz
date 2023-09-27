4c59294a66311fa8b5d3ce3f758d6d29be258b58
Emem Isaac <me@ememisaac.com>
2023-09-27 20:36:35 +0100

    Adds the question command validator

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Commands/CreateQuestion/CreateQuestionCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using EmemIsaac.QuizManager.Application.Common;
+using EmemIsaac.QuizManager.Domain.Entities;
+using FluentValidation;
+
+namespace EmemIsaac.QuizManager.Application.Features.Questions.Commands.CreateQuestion
+{
+    public class CreateQuestionCommandValidator : AbstractValidator<CreateQuestionCommand>
+    {
+        public CreateQuestionCommandValidator()
+        {
+            RuleFor(p => p.Text)
+               .NotEmpty().WithMessage(ValidationMessages.RequiredMessage)
+               .NotNull()
+               .MinimumLength(Question.TextMinLength).WithMessage(ValidationMessages.MinimumLengthMessage(Question.TextMinLength))
+               .MaximumLength(Question.TextMaxLength).WithMessage(ValidationMessages.MaximumLengthMessage(Question.TextMaxLength));
+
+            RuleFor(p => p.ImageUri)
+                .MaximumLength(Question.ImageUriMaxLength).WithMessage(ValidationMessages.MaximumLengthMessage(Question.ImageUriMaxLength));
+        }
+    }
+}
