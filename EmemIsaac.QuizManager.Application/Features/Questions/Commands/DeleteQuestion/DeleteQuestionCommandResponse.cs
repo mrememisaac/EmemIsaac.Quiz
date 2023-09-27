15634037aa027c80f3bf49342411b9fa504cc4f7
Emem Isaac <me@ememisaac.com>
2023-09-27 20:39:57 +0100

    Adds the delete question response

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Commands/DeleteQuestion/DeleteQuestionCommandResponse.cs
@@ -0,0 +1,13 @@
+﻿using EmemIsaac.QuizManager.Application.Responses;
+
+namespace EmemIsaac.QuizManager.Application.Features.Questions.Commands.DeleteQuestion
+{
+    public class DeleteQuestionCommandResponse : BaseResponse
+    {
+        public DeleteQuestionCommandResponse(bool success = true, string message = "") : base(success, message)
+        {
+        }
+
+        public DeleteQuestionCommandResponse(IList<string> errors) : base(errors) { }
+    }
+}
