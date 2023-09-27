25adb1ebbaf42839d7b869a080b87058cf4cda81
Emem Isaac <me@ememisaac.com>
2023-09-27 20:41:15 +0100

    Adds the update option dto

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Commands/UpdateQuestion/UpdateOptionCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace EmemIsaac.QuizManager.Application.Features.Questions.Commands.UpdateQuestion
+{
+    public class UpdateOptionCommand
+    {
+        public string Text { get; set; } = string.Empty;
+
+        public string ImageUri { get; set; } = string.Empty;
+
+        public bool IsAnswer { get; set; }
+    }
+}
