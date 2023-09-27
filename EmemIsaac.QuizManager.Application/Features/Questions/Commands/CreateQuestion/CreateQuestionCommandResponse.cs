18783dfc6feb88c6c2aaeaf65bd2f1243da96821
Emem Isaac <me@ememisaac.com>
2023-09-27 20:35:45 +0100

    Adds the create question command response

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Commands/CreateQuestion/CreateQuestionCommandResponse.cs
@@ -0,0 +1,16 @@
+﻿using EmemIsaac.QuizManager.Application.Responses;
+
+namespace EmemIsaac.QuizManager.Application.Features.Questions.Commands.CreateQuestion
+{
+    public class CreateQuestionCommandResponse : BaseResponse
+    {
+        public CreateQuestionDto? Question { get; }
+
+        public CreateQuestionCommandResponse(bool success = true, string message = "", CreateQuestionDto? question = null) : base(success, message)
+        {
+            Question = question;
+        }
+
+        public CreateQuestionCommandResponse(IList<string> errors) : base(errors) { }
+    }
+}
