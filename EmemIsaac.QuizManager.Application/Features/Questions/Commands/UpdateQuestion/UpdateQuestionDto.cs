c50fe0f1f2fed366755544e2cd99401769f8d927
Emem Isaac <me@ememisaac.com>
2023-09-27 20:56:04 +0100

    Adds a dto for question update

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Commands/UpdateQuestion/UpdateQuestionDto.cs
@@ -0,0 +1,15 @@
+﻿namespace EmemIsaac.QuizManager.Application.Features.Questions.Commands.UpdateQuestion
+{
+    public class UpdateQuestionDto
+    {
+        public Guid QuizId { get; set; }
+
+        public Guid Id { get; set; }
+
+        public string Text { get; set; } = string.Empty;
+
+        public string ImageUri { get; set; } = string.Empty;
+
+        private List<UpdateOptionCommandResponse> Options { get; set; } = new();
+    }
+}
