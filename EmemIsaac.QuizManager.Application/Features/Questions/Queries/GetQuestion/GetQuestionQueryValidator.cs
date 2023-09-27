1cb7f3a2f0dbba86c6e416161f62605c54565e6e
Emem Isaac <me@ememisaac.com>
2023-09-27 20:59:15 +0100

    Adds the get question query validator

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Queries/GetQuestion/GetQuestionQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace EmemIsaac.QuizManager.Application.Features.Questions.Queries.GetQuestion
+{
+    public class GetQuestionQueryValidator : AbstractValidator<GetQuestionQuery>
+    {
+        public GetQuestionQueryValidator()
+        {
+            RuleFor(p => p.QuestionId)
+                .NotEmpty()
+                .WithMessage(Common.ValidationMessages.RequiredMessage);
+        }
+    }
+}
