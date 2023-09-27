9b9820f0a6377e75ea7575e5682bcae3fee28df5
Emem Isaac <me@ememisaac.com>
2023-09-27 20:41:30 +0100

    Adds the update question response

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Commands/UpdateQuestion/UpdateOptionCommandResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace EmemIsaac.QuizManager.Application.Features.Questions.Commands.UpdateQuestion
+{
+    public class UpdateOptionCommandResponse
+    {
+        public Guid Id { get; set; }
+
+        public string Text { get; set; } = string.Empty;
+
+        public string ImageUri { get; set; } = string.Empty;
+
+        public bool IsAnswer { get; set; }
+    }
+}
