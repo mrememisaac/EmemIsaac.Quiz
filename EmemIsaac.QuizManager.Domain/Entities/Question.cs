b9f11b139d64af331fbde0346f1bfab195d1e359
Emem Isaac <me@ememisaac.com>
2023-09-27 21:03:02 +0100

    Adds validation properties

--- EmemIsaac.QuizManager.Domain/Entities/Question.cs
+++ EmemIsaac.QuizManager.Domain/Entities/Question.cs
@@ -4,6 +4,12 @@ namespace EmemIsaac.QuizManager.Domain.Entities;
 
 public class Question : AuditableEntity
 {
+    public const int TextMinLength = 5;
+    public const int TextMaxLength = 250;
+
+    public const int ImageUriMinLength = 5;
+    public const int ImageUriMaxLength = 350;
+
     public Guid QuizId {get; set; }
     
     public Guid Id {get; set;}
@@ -12,4 +18,7 @@ public class Question : AuditableEntity
 
     public string ImageUri { get; set; } = string.Empty;
 
+    private List<Option> _options { get; set; } = new();
+
+    public virtual IReadOnlyList<Option> Options => _options.AsReadOnly();
 }
