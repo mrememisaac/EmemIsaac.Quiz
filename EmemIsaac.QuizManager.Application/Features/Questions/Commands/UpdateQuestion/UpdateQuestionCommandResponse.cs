1d1db7b7fb7231e94f92ba982dba5b6a4952780d
Emem Isaac <me@ememisaac.com>
2023-09-27 20:43:05 +0100

    adds a response for question update command

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Commands/UpdateQuestion/UpdateQuestionCommandResponse.cs
@@ -0,0 +1,16 @@
+﻿using EmemIsaac.QuizManager.Application.Responses;
+
+namespace EmemIsaac.QuizManager.Application.Features.Questions.Commands.UpdateQuestion
+{
+    public class UpdateQuestionCommandResponse : BaseResponse
+    {
+        public UpdateQuestionDto? Question { get; }
+
+        public UpdateQuestionCommandResponse(bool success = true, string message = "", UpdateQuestionDto? question = null) : base(success, message)
+        {
+            Question = question;
+        }
+
+        public UpdateQuestionCommandResponse(IList<string> errors) : base(errors) { }
+    }
+}
