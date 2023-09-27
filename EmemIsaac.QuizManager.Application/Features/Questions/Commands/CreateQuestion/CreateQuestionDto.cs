4848b78db9ef09494180683c932ff96bab682be6
Emem Isaac <me@ememisaac.com>
2023-09-27 20:36:57 +0100

    Adds the create question dto

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Commands/CreateQuestion/CreateQuestionDto.cs
@@ -0,0 +1,15 @@
+﻿namespace EmemIsaac.QuizManager.Application.Features.Questions.Commands.CreateQuestion
+{
+    public class CreateQuestionDto
+    {
+        public Guid QuizId { get; set; }
+
+        public Guid Id { get; set; }
+
+        public string Text { get; set; } = string.Empty;
+
+        public string ImageUri { get; set; } = string.Empty;
+
+        private List<CreateOptionCommandResponse> Options { get; set; } = new();
+    }
+}
