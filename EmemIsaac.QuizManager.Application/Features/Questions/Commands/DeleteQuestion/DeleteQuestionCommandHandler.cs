2a1fc2fb274f46e450d6d38b28a24e54f748a7b6
Emem Isaac <me@ememisaac.com>
2023-09-27 20:38:06 +0100

    Implements the delete question feature

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Commands/DeleteQuestion/DeleteQuestionCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using EmemIsaac.QuizManager.Application.Contracts.Persistence;
+using EmemIsaac.QuizManager.Domain.Entities;
+using MediatR;
+
+namespace EmemIsaac.QuizManager.Application.Features.Questions.Commands.DeleteQuestion
+{
+    public class DeleteQuestionCommandHandler : IRequestHandler<DeleteQuestionCommand, DeleteQuestionCommandResponse>
+    {
+        private readonly IQuestionsRepository _questionsRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteQuestionCommandHandler(IQuestionsRepository questionsRepository, IMapper mapper)
+        {
+            _questionsRepository = questionsRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<DeleteQuestionCommandResponse> Handle(DeleteQuestionCommand request, CancellationToken cancellationToken)
+        {
+            var question = _mapper.Map<Question>(request);
+
+            var validator = new DeleteQuestionCommandValidator();
+            var validationResult = await validator.ValidateAsync(request);
+            if (validationResult.Errors.Count > 0)
+                throw new Common.ValidationException(validationResult);
+
+            await _questionsRepository.DeleteAsync(question);
+            return new DeleteQuestionCommandResponse();
+        }
+    }
+}
