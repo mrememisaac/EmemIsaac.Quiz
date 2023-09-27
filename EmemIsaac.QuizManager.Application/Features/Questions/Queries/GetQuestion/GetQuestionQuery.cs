6c7a478a6673d7a3b85e56da0e715f35580bad4f
Emem Isaac <me@ememisaac.com>
2023-09-27 20:58:21 +0100

    Adds the get question query

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Queries/GetQuestion/GetQuestionQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace EmemIsaac.QuizManager.Application.Features.Questions.Queries.GetQuestion
+{
+    public class GetQuestionQuery : IRequest<GetQuestionQueryResponse>
+    {
+        public Guid QuizId { get; set; }
+
+        public Guid QuestionId { get; set; }
+
+    }
+}
