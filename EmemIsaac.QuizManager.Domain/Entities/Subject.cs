70bc1dc5a6be3ac08d68c6cdfeed439a7d31835f
Emem Isaac <me@ememisaac.com>
2023-09-27 21:06:50 +0100

    Adds the subject entity - quizzes belong to subjects

---
+++ EmemIsaac.QuizManager.Domain/Entities/Subject.cs
@@ -0,0 +1,14 @@
+﻿using EmemIsaac.QuizManager.Domain.Common;
+
+namespace EmemIsaac.QuizManager.Domain.Entities;
+
+public class Subject : AuditableEntity
+{
+    public const int NameMinLength = 3;
+
+    public const int NameMaxLength = 250;
+
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+}
