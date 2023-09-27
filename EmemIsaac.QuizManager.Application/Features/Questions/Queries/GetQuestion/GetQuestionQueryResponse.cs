f92837b8d8391e51ca637ad3c82d1dfc1ded2c43
Emem Isaac <me@ememisaac.com>
2023-09-27 20:59:01 +0100

    Adds the get question query response

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Queries/GetQuestion/GetQuestionQueryResponse.cs
@@ -0,0 +1,16 @@
+﻿using EmemIsaac.QuizManager.Application.Responses;
+
+namespace EmemIsaac.QuizManager.Application.Features.Questions.Queries.GetQuestion
+{
+    public class GetQuestionQueryResponse : BaseResponse
+    {
+        public GetQuestionDto Question { get; set; }
+
+        public GetQuestionQueryResponse(GetQuestionDto question, bool status = true, string message = "") : base(status, message)
+        {
+            Question = question;
+        }
+
+        public GetQuestionQueryResponse(IList<string> errors) : base(errors) { }
+    }
+}
