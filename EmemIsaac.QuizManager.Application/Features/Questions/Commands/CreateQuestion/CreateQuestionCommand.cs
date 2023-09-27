220c7a1d67bd5dd12093673b8f7616d542df1a85
Emem Isaac <me@ememisaac.com>
2023-09-27 20:27:57 +0100

    adds the create question command

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Commands/CreateQuestion/CreateQuestionCommand.cs
@@ -0,0 +1,17 @@
+﻿using EmemIsaac.QuizManager.Application.Features.Quizzes.Commands.CreateQuiz;
+using MediatR;
+
+namespace EmemIsaac.QuizManager.Application.Features.Questions.Commands.CreateQuestion
+{
+
+    public class CreateQuestionCommand : IRequest<CreateQuestionCommandResponse>
+    {
+        public Guid QuizId { get; set; }
+
+        public string Text { get; set; } = string.Empty;
+
+        public string ImageUri { get; set; } = string.Empty;
+
+        private List<CreateOptionCommand> Options { get; set; } = new();
+    }
+}
