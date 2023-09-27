e5be72c98653ac84bd495af4f3d3195c6a347ee9
Emem Isaac <me@ememisaac.com>
2023-09-27 20:43:23 +0100

    Adds a validator for question update command

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Commands/UpdateQuestion/UpdateQuestionCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using EmemIsaac.QuizManager.Domain.Entities;
+using FluentValidation;
+
+namespace EmemIsaac.QuizManager.Application.Features.Questions.Commands.UpdateQuestion
+{
+    public class UpdateQuestionCommandValidator : AbstractValidator<UpdateQuestionCommand>
+    {
+        public UpdateQuestionCommandValidator()
+        {
+            RuleFor(p => p.Text)
+               .NotEmpty().WithMessage(Common.ValidationMessages.RequiredMessage)
+               .NotNull()
+               .MinimumLength(Question.TextMinLength).WithMessage(Common.ValidationMessages.MinimumLengthMessage(Question.TextMinLength))
+               .MaximumLength(Question.TextMaxLength).WithMessage(Common.ValidationMessages.MaximumLengthMessage(Question.TextMaxLength));
+
+            RuleFor(p => p.ImageUri)
+                .MaximumLength(Question.ImageUriMaxLength).WithMessage(Common.ValidationMessages.MaximumLengthMessage(Question.ImageUriMaxLength));
+        }
+    }
+}
