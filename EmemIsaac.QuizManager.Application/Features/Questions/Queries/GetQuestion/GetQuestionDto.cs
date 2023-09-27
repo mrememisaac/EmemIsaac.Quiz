045466b2dba27959fb2870936d08137827a7f483
Emem Isaac <me@ememisaac.com>
2023-09-27 20:57:52 +0100

    adds the get question dto

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Queries/GetQuestion/GetQuestionDto.cs
@@ -0,0 +1,15 @@
+﻿using EmemIsaac.QuizManager.Application.Features.Questions.Commands.CreateQuestion;
+
+namespace EmemIsaac.QuizManager.Application.Features.Questions.Queries.GetQuestion
+{
+    public class GetQuestionDto
+    {
+        public Guid QuizId { get; set; }
+
+        public string Text { get; set; } = string.Empty;
+
+        public string ImageUri { get; set; } = string.Empty;
+
+        private List<CreateOptionCommand> Options { get; set; } = new();
+    }
+}
