63e1d571f261aa8f38ff8bd2651979a82d2f49ed
Emem Isaac <me@ememisaac.com>
2023-09-27 20:37:19 +0100

    Adds the delete question command

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Commands/DeleteQuestion/DeleteQuestionCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace EmemIsaac.QuizManager.Application.Features.Questions.Commands.DeleteQuestion
+{
+
+    public class DeleteQuestionCommand : IRequest<DeleteQuestionCommandResponse>
+    {
+        public Guid QuestionId { get; set; }
+
+    }
+}
