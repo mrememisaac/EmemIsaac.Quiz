b890920e7bc9b10baea6cf01e1db065f9cecce1f
Emem Isaac <me@ememisaac.com>
2023-09-27 20:26:41 +0100

    Adds the create option command response

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Commands/CreateQuestion/CreateOptionCommandResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace EmemIsaac.QuizManager.Application.Features.Questions.Commands.CreateQuestion
+{
+    public class CreateOptionCommandResponse
+    {
+        public Guid Id { get; set; }
+
+        public string Text { get; set; } = string.Empty;
+
+        public string ImageUri { get; set; } = string.Empty;
+
+        public bool IsAnswer { get; set; }
+    }
+}
