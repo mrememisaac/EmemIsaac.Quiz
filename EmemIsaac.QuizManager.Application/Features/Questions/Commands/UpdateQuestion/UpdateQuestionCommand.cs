f7eccb7b4dbcb5e8a5493cace9e46d2bf3532872
Emem Isaac <me@ememisaac.com>
2023-09-27 20:41:48 +0100

    adds the question update command

---
+++ EmemIsaac.QuizManager.Application/Features/Questions/Commands/UpdateQuestion/UpdateQuestionCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace EmemIsaac.QuizManager.Application.Features.Questions.Commands.UpdateQuestion
+{
+
+    public class UpdateQuestionCommand : IRequest<UpdateQuestionCommandResponse>
+    {
+        public Guid QuizId { get; set; }
+
+        public Guid QuestionId { get; set; }
+        
+        public string Text { get; set; } = string.Empty;
+
+        public string ImageUri { get; set; } = string.Empty;
+
+        private List<UpdateOptionCommand> Options { get; set; } = new();
+    }
+}
